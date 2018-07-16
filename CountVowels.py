b133a5ce206ace4895003c33b8ddac0685632133
mjschoen <mjs3401@gmail.com>
2018-07-15 17:23:46 -0700

    WW29.Sun

--- CountVowels.py
+++ CountVowels.py
@@ -3,11 +3,7 @@ Count Vowels – Enter a string and the program counts the number of vowels in t
 report a sum of each vowel found.
 """
 
-vowelCount = {'A': 0,
-    'E': 0,
-    'I': 0,
-    'O': 0,
-    'U': 0}
+vowelCount = {'A': 0, 'E': 0, 'I': 0, 'O': 0, 'U': 0}
 
 userInput = input("Enter string to get vowel count: ").upper()
 
